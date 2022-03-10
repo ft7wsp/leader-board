72b049b8734d96db7ecf97e4aa15716e007df0d1
radhi <tliliradhi8@gmail.com>
2022-03-10 13:20:21 +0100

    end

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState();
+  const [winner, setWinner] = useState();
+  const [winnerScore, setWinnerScore] = useState();
 
-  useEffect(function () {
-
+  useEffect(function(){
     axios
       .get("https://cyberleague.securinetsesprit.com/api/v1/scoreboard/top/10")
       .then((res) => {
@@ -14,59 +15,80 @@ function App() {
           (el) => res.data.data[el]
         );
         setData(apiData);
-        console.log(data, typeof(data), apiData, typeof(apiData));
+        const a1 = data.map((el, i) => {
+          return el.solves.map((el) => el);
+
+          // return data[el].solves
+        });
+        setWinner(data[0]);
+
+        const values = []
+        for (let i = 0; i < 1; i++) {
+          const el = a1[i];
+          let fv = 0;
+          for (let e = 0; e < el.length; e++) {
+            const v = el[e];
+    
+            fv += v.value;
+    
+            values[i] = fv;
+          }
+        }
+        
+        setWinnerScore(values[0])
+
       });
-  }, []);
+  });
 
   
+  
   const display = () => {
-    const values = Array(data.length)
-
-    console.log(values);
-    const a1 = data.map((el,i) => {
-      return (el.solves.map(el => el ))
-      
-      // return data[el].solves
-    })
+    const values = Array(data.length);
     
+    const a1 = data.map((el, i) => {
+      return el.solves.map((el) => el);
+    });
+
+
     for (let i = 0; i < a1.length; i++) {
       const el = a1[i];
-      let fv = 0
+      let fv = 0;
       for (let e = 0; e < el.length; e++) {
         const v = el[e];
- 
 
-        fv = fv + v.value
-       
-        values[i] = fv
+        fv += v.value;
+
+        values[i] = fv;
       }
     }
 
-    const randomEmoji = () => {
-      const emojis = ['👏', '👍', '🙌', '🤩', '🔥', '⭐️', '🏆', '💯'];
-      let randomNumber = Math.floor(Math.random() * emojis.length);
-      return emojis[randomNumber];
+    
+    const randomEmoji = (i) => {
+      const emojis = ["🏆","💯","⭐️","🔥",    "🤩",  "👏", "👍", "🙌", ];
+      return emojis[i]
     };
-    console.log(values);
     return (
       <div>
-        {data.map((el,i) => {
+        {data.map((el, i) => {
           return (
             <div key={i}>
               <div className="c-list__grid">
-                <div className="c-flag c-place u-bg--transparent">rank{i+1}   {el.id}</div>
+                <div className="c-flag c-place u-bg--transparent u-ml--24 ">
+                  rank{i + 1} 
+                </div>
                 <div className="c-media">
                   <div className="c-media__content">
-                    <div className="c-media__title">{el.name}</div>
+                    <div className="c-media__title u-ml--24 ">{el.name}</div>
                   </div>
                 </div>
                 <div className="u-text--right c-kudos">
                   <div className="u-mt--8">
-                  <strong>{values[i]} {randomEmoji()}</strong>
-                    </div>
+                    <strong>
+                      {values[i]} {randomEmoji(i)}
+                    </strong>
+                  </div>
                 </div>
               </div>
-              
             </div>
           );
         })}
@@ -74,23 +96,28 @@ function App() {
     );
   };
 
+  
   return (
     <div className="App">
       <div className="l-wrapper">
-        <div className="c-header">
-          <img
-            className="c-logo"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/813538/km-logo-color.svg"
-            draggable="false"
-          />
-          <button className="c-button c-button--primary">Send Kudos</button>
-        </div>
+       
         <div className="l-grid">
           <div className="l-grid__item l-grid__item--sticky">
             <div className="c-card u-bg--light-gradient u-text--dark"></div>
             <div className="c-card">
               <div className="c-card__body">
-                <div className="u-text--center" id="winner"></div>
+                <div className="u-text--center" id="winner">
+                  {winner && winnerScore ? (
+                    <>
+
+	<div className="u-text-small u-text--medium u-mb--16">Top Scorer</div>
+	<h3 className="u-mt--16">{winner.name}🏆</h3>
+	<span className="u-text--teal u-text--small">{winnerScore}</span>
+                    </>
+                  ) : (
+                    <div>could not found winner</div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
@@ -98,9 +125,7 @@ function App() {
             <div className="c-card">
               <div className="c-card__header">
                 <h3>Received Kudos</h3>
-                <select className="c-select">
-                  <option>Sunday, Feb. 23 - Sunday, Feb. 30</option>
-                </select>
+                
               </div>
               <div className="c-card__body">
                 <ul className="c-list" id="list">
@@ -110,10 +135,10 @@ function App() {
                         Rank
                       </div>
                       <div className="u-text--left u-text--small u-text--medium">
-                        Team Member
+                        Team 
                       </div>
                       <div className="u-text--right u-text--small u-text--medium">
-                        # of Kudos
+                        # scores
                       </div>
                     </div>
                   </li>
@@ -122,7 +147,7 @@ function App() {
               {data ? (
                 <div>{display()}</div>
               ) : (
-                <div>data does not exist yet</div>
+                <div>data does not yet exist </div>
               )}
             </div>
           </div>
